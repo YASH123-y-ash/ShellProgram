0affeeaa082746abd2f389dbe818456db19e3188
Yash-y-ash <wardhan696@gmail.com>
2021-02-23 10:16:38 +0530

    Added: selection statement

---
+++ SequenceStatement/SumAvg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x 
+read -p "first number::"$((RANDOM%10+10))
+read -p "second number::"$((RANDOM%10+10))
+read -p "third number::"$((RANDOM%10+10))
+read -p "fourth number::"$((RANDOM%10+10))
+read -p "fifth number::"$((RANDOM%10+10))
+
+
