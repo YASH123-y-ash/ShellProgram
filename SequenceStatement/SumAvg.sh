5059928b99f10a83e346c93490ca3499b00fb321
Yash-y-ash <wardhan696@gmail.com>
2021-02-24 16:00:41 +0530

    Added:sum and average of random number

--- SequenceStatement/SumAvg.sh
+++ SequenceStatement/SumAvg.sh
@@ -1,8 +1,12 @@
-#!/bin/bash -x 
-read -p "first number::"$((RANDOM%10+10))
-read -p "second number::"$((RANDOM%10+10))
-read -p "third number::"$((RANDOM%10+10))
-read -p "fourth number::"$((RANDOM%10+10))
-read -p "fifth number::"$((RANDOM%10+10))
+#!/bin/bash -x
+num1=$((RANDOM%10+10))
+num2=$((RANDOM%10+10))
+num3$((RANDOM%10+10))
+num4$((RANDOM%10+10))
+num5=$((RANDOM%10+10))
+sum=$(($num1+$num2+$num3+$num4+$num5))
+echo "sum of all number is :: $sum"
+avg=$(($sum/5))
+echo "average of all number is :: $avg"
 
 
